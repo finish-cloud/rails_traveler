73fbccdd87f442768889c74b6c2b0a1d033f91d1
masaya <hope.is.a.waking.dream.8.1@gmail.com>
2021-02-08 14:22:27 +0900

    コントローラにcreateアクションを追加→入力したデータをモデルに保存する処理

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -11,6 +11,8 @@ class RoomsController < ApplicationController
   end
 
   def create
+    room = Room.create!(name: room_params[:name], introduction: room_params[:introduction], charge: room_params[:charge], address: room_params[:address], image: room_params[:image], user_id: :current_user.id)
+    redirect_to room
   end
 
   def edit
@@ -20,5 +22,9 @@ class RoomsController < ApplicationController
   end
 
   def destroy
+  end
+
+  def room_params
+    params.require(:room).permit(:name, :introduction, :charge, :address, :image)
   end
 end
